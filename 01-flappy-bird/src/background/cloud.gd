e6e05efa6d05a92516a42c03253be77cd83043ab
Thomas Liebregts <thomas_liebregts@hotmail.com>
2025-01-29 12:38:28 +0100

    feat: add clouds

---
+++ 01-flappy-bird/src/background/cloud.gd
@@ -0,0 +1,32 @@
+@tool
+extends Sprite2D
+
+@export var cloud_textures: Array[Texture2D]
+const MIN_SPEED = 25
+const MAX_SPEED = 85
+const MIN_SCALE = 0.5
+const MAX_SCALE = 2
+
+var speed: float
+var image_scale: float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var cloud_index = randi_range(0, cloud_textures.size() - 1)
+	self.texture = cloud_textures[cloud_index]
+	speed = randf_range(MIN_SPEED, MAX_SPEED)
+	
+	var new_scale = ((speed - MIN_SPEED) * MIN_SCALE) / (MAX_SPEED - MIN_SPEED) + MIN_SCALE
+	scale = Vector2(new_scale, new_scale)
+	z_index = new_scale * 100
+	# 25 = 0.5
+	# 200 = 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if Engine.is_editor_hint():
+		return
+		
+	position.x -= speed * delta
+	pass
--- 01-flappy-bird/src/player/player.gd
+++ 01-flappy-bird/src/player/player.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
